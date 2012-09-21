ba72a6849960a87a094c5ba6b0243219da9c6f7a
Jamie Mason <siunmaru@gmail.com>
2012-09-21 20:42:03 +0100

    added references to each language to the response object, to support translating values such as the current URL to other languages

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var authRoutes;
 var languages;
 var register;
 var site;
+var langs;
 
 exports.init = function(app, langName) {
 
@@ -12,22 +13,35 @@ exports.init = function(app, langName) {
     languages = require('../modules/languages');
     register = require('./register');
     site = require('./site');
+    langs = {
+      nlNl: languages.get('nl-nl'),
+      enGb: languages.get('en-gb')
+    };
   }
 
   var lang = languages.get(langName);
 
-  function activateLanguage(req, res, next) {
+  function provideLocals(index, req, res, next) {
+    var locals = res.locals;
+
+    // activate language
     languages.select(langName);
-    res.locals.lang = lang;
+    locals.lang = lang;
+    locals.langs = langs;
+
+    // misc
+    locals.urlIndex = index;
+
+    // continue
     next();
   }
 
-  app.get(lang('/.route'), activateLanguage, site.index);
-  app.get(lang('/login.route'), activateLanguage, site.login);
-  app.post(lang('/login.route'), activateLanguage, authRoutes.postLogin);
-  app.get(lang('/logout.route'), activateLanguage, authRoutes.logout);
-  app.get(lang('/register.route'), activateLanguage, register.get);
-  app.post(lang('/register.route'), activateLanguage, register.post);
-  app.get(lang('/restricted.route'), activateLanguage, authentication.restrictRoute, site.restricted);
+  app.get  (lang('/.route'),           provideLocals.bind(app, '/.route'),           site.index);
+  app.get  (lang('/login.route'),      provideLocals.bind(app, '/login.route'),      site.login);
+  app.post (lang('/login.route'),      provideLocals.bind(app, '/login.route'),      authRoutes.postLogin);
+  app.get  (lang('/logout.route'),     provideLocals.bind(app, '/logout.route'),     authRoutes.logout);
+  app.get  (lang('/register.route'),   provideLocals.bind(app, '/register.route'),   register.get);
+  app.post (lang('/register.route'),   provideLocals.bind(app, '/register.route'),   register.post);
+  app.get  (lang('/restricted.route'), provideLocals.bind(app, '/restricted.route'), authentication.restrictRoute, site.restricted);
 
 };
