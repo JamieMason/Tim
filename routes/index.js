0b1213592548f35d83a956dac0db7703d1e43d04
Jamie Mason <siunmaru@gmail.com>
2012-09-21 15:26:34 +0100

    routes are defined for each language, and languages are toggled to on browsing to one of it's routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var site = require('./site');
 var register = require('./register');
 var authentication = require('../modules/authentication');
-var lang = require('../modules/languages').get();
 
-exports.init = function(app) {
-  app.get(lang('/.route'), site.index);
-  app.get(lang('/login.route'), site.login);
-  app.get(lang('/register.route'), register.get);
-  app.post(lang('/register.route'), register.post);
-  app.get(lang('/restricted.route'), authentication.restrictRoute, site.restricted);
+exports.init = function(app, lang) {
+
+  function activateLanguage(req, res, next) {
+    res.locals.lang = lang;
+    next();
+  }
+
+  app.get(lang('/.route'), activateLanguage, site.index);
+  app.get(lang('/login.route'), activateLanguage, site.login);
+  app.get(lang('/register.route'), activateLanguage, register.get);
+  app.post(lang('/register.route'), activateLanguage, register.post);
+  app.get(lang('/restricted.route'), activateLanguage, authentication.restrictRoute, site.restricted);
+
 };
