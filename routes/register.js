e607378a0b61cfe95cdf12af6e49de5014ba86a7
Jamie Mason <siunmaru@gmail.com>
2012-09-23 21:10:29 +0100

    implemented /register POST route

--- routes/register.js
+++ routes/register.js
@@ -1,3 +1,6 @@
+var messaging = require('../modules/messaging');
+var registration = require('../modules/registration');
+
 exports.get = function(req, res) {
   res.render('register', {
     title: res.locals.lang('/register.title')
@@ -5,5 +8,24 @@ exports.get = function(req, res) {
 };
 
 exports.post = function(req, res) {
-  res.send('@TODO handle posted reg form');
+  var body = req.body;
+
+  registration.register({
+    name: body.name,
+    email: body.email,
+    password: body.password,
+    password2: body.password2,
+    onComplete: function(err, user) {
+      if (err) {
+        messaging.queue('error', req, err);
+        res.redirect(res.locals.lang('/register.route'));
+      } else {
+        messaging.queue('success', req, res.locals.lang('REGISTER_SUCCESS', {
+          name: body.name,
+          email: body.email
+        }));
+        res.redirect(res.locals.lang('/.route'));
+      }
+    }
+  });
 };
